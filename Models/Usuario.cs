943417ad87159b0d80517b92fe6a1529b666989a
Bruno Gois <bbgbruno@gmail.com>
2025-05-23 15:16:48 -0300

    removendo Arquivos

--- Models/Usuario.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BoardGamesMarket.Models;
-
-public class Usuario
-{
-    public int Id { get; set; }
-    public string Nome { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string SenhaHash { get; set; } = string.Empty;
-}
